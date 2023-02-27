32a36b3341e1db360bc32391e65ab00b56127985
mmadrz <mmadrzfathi@gmail.com>
2023-02-27 14:58:24 +0330

    upd

--- pages/1_📷_Timelapse.py
+++ pages/1_📷_Timelapse.py
@@ -47,7 +47,8 @@ end_date = st.date_input("Select the end date:")
 row1_col1, row1_col2 = st.columns([2, 1])
 
 if st.session_state.get("zoom_level") is None:
-    st.session_state["zoom_level"] = 4
+    st.session_state["zoom_level"] = 1
+
 
 st.session_state["ee_asset_id"] = None
 st.session_state["bands"] = None
