968537db4aa51ab4f8b61150ef52bba1ff07a052
mmadrz <mmadrzfathi@gmail.com>
2023-02-27 18:03:45 +0330

    upd

--- pages/1_📷_Timelapse.py
+++ pages/1_📷_Timelapse.py
@@ -118,8 +118,8 @@ if file_path.lower().endswith(".kml"):
     gdf = gpd.read_file(file_path, driver="KML")
 else:
     gdf = gpd.read_file(file_path)
+
 lon, lat = leafmap.gdf_centroid(gdf)
- 
 m = leafmap.Map(center=(lat, lon), draw_export=True)
 m.add_gdf(gdf, layer_name=layer_name)
 
