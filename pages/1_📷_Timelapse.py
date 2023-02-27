bfc9e716930210c74dda55bf27a7d4521cb8155c
mmadrz <mmadrzfathi@gmail.com>
2023-02-27 14:57:08 +0330

    upd

--- pages/1_📷_Timelapse.py
+++ pages/1_📷_Timelapse.py
@@ -55,15 +55,6 @@ st.session_state["palette"] = None
 st.session_state["vis_params"] = None
 
 with st.echo():
-        filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
         m = leafmap.Map(center=[40, -100], zoom=4, tiles="stamentoner")
-        m.add_heatmap(
-            filepath,
-            latitude="latitude",
-            longitude="longitude",
-            value="pop_max",
-            name="Heat map",
-            radius=20,
-        )
 m.to_streamlit(height=700)
 
