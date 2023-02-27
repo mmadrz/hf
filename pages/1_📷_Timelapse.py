fc3e61440f804e322a85e3ca657fc1fe479737cf
mmadrz <mmadrzfathi@gmail.com>
2023-02-27 14:02:42 +0330

    upd

--- pages/1_📷_Timelapse.py
+++ pages/1_📷_Timelapse.py
@@ -16,7 +16,19 @@ st.set_page_config(layout="wide")
 warnings.filterwarnings("ignore")
 
 
-
+collection = st.selectbox(
+            "Select a satellite image collection: ",
+            [
+                "Landsat TM-ETM-OLI Surface Reflectance",
+                "Sentinel-2 MSI Surface Reflectance",
+                "Geostationary Operational Environmental Satellites (GOES)",
+                "MODIS Vegetation Indices (NDVI/EVI) 16-Day Global 1km",
+                "MODIS Gap filled Land Surface Temperature Daily",
+                "MODIS Ocean Color SMI",
+                "USDA National Agriculture Imagery Program (NAIP)",
+            ],
+            index=1,
+        )
 json_data = st.secrets["json_data"]
 service_account = st.secrets["service_account"]
 # Preparing values
@@ -30,3 +42,13 @@ ee.Initialize(credentials)
 
 start_date = st.date_input("Select the start date:")
 end_date = st.date_input("Select the end date:")
+
+row1_col1, row1_col2 = st.columns([2, 1])
+
+if st.session_state.get("zoom_level") is None:
+    st.session_state["zoom_level"] = 4
+
+st.session_state["ee_asset_id"] = None
+st.session_state["bands"] = None
+st.session_state["palette"] = None
+st.session_state["vis_params"] = None
